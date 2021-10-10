ccadd542a78436553d16adbb98faaffcca308508
Ignacio Losiggio <iglosiggio@dc.uba.ar>
2021-10-10 00:49:22 -0300

    No usar sqrt xq sí (?

--- src/pca.cpp
+++ src/pca.cpp
@@ -20,11 +20,10 @@ void PCA::fit(Matrix X){
   // corresponde a una fila de $X$ y tienen media cero
   // (i.e., $X_i := (x^{(i) - \mu)$).
   for (int i = 0; i < X.rows(); i++) X.row(i) -= means;
-  X /= sqrt(X.rows() - 1);
 
   // Diagonalizamos la matriz de covarianzas $M_x = \frac{X^tX}{n - 1}$. La
   // matriz $V$ (ortogonal) contiene los autovectores de $M_x$.
-  Matrix covariance = X.transpose() * X;
+  Matrix covariance = (X.transpose() * X) / (X.rows() - 1);
   tuple<Vector, Matrix> eigens = get_first_eigenvalues(covariance, alpha);
   this->T = get<1>(eigens);
 }
